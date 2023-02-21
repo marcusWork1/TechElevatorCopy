59d435ea493a5601ec1346a0cca4a04b72daac68
Marcus Patterson <mpatterson8oh@gmail.com>
2023-02-21 09:00:54 -0500

    exercise

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Employee.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Employee.java
@@ -2,4 +2,72 @@ package com.techelevator;
 
 public class Employee {
 
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String fullName=firstName+" "+lastName ;
+    private String	department;
+    private double annualSalary;
+
+    public Employee(int employeeId, String firstName, String lastName, double annualSalary) {
+
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.annualSalary= annualSalary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+  public void raiseSalary(double percent) {
+      annualSalary += annualSalary * percent / 100;
+
+  }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", department='" + department + '\'' +
+                ", annualSalary=" + annualSalary +
+                '}';
+    }
 }
