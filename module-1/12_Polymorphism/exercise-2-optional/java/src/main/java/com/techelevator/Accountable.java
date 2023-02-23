819dbd8a23f2ece0773480a361a7cec38b765a6f
frank fella <frank.fella@techelevator.com>
2023-02-23 05:14:57 -0700

    Instructor Push 02/23/2023 05:14:56

---
+++ module-1/12_Polymorphism/exercise-2-optional/java/src/main/java/com/techelevator/Accountable.java
@@ -0,0 +1,10 @@
+package com.techelevator;
+
+/**
+ * Accountable
+ *
+ * if you want be an Accountable class you must provide these methods
+ */
+public interface Accountable {
+    int getBalance();
+}
---
+++ module-1/12_Polymorphism/exercise-2-optional/java/src/main/java/com/techelevator/CreditCardAccount.java
@@ -0,0 +1,23 @@
+package com.techelevator;
+
+/**
+ * CreditCardAccount
+ */
+public class CreditCardAccount implements Accountable {  // This is a type of Accountable class
+	
+/*******************************************************************
+ * Place the code for  your CreditCard class here
+ *******************************************************************/
+
+
+    public int getBalance() {
+    	//***************************************************************
+    	// This should return the negative of the debt data member
+    	//***************************************************************
+    	
+        return -0;
+    }
+
+ 
+
+}
