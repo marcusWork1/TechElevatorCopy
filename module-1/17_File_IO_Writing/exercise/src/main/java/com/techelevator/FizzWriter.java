7ed05caa55cc75c8f22adbbb592d745d9e3862a0
Marcus Patterson <mpatterson8oh@gmail.com>
2023-03-02 08:27:56 -0500

    files

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -22,29 +22,55 @@ public class FizzWriter {
 		System.out.println("what is the destination file?");
 		String destinationFile = userInput.nextLine();
 
-		// create new file with given name
-		try {
-			PrintWriter newFile = new PrintWriter(destinationFile);
 
-			// loop from 1-300
-			for (int i = 0; i < 300; i++)
+		try {
+// create a new file with given filename
+			PrintWriter writer = new PrintWriter(destinationFile);
+
+// loop from 1 to 300(inclusive)
+			for(int n=1;n<=300;n++)
 			{
-				if(i%3 == 0 && i%5 == 0) { // i is divisible by 3 and 5
-					newFile.println("FizzBuzz");
-				} else if (i%3 == 0) { // i is divisible by 3
-					newFile.println("Fizz");
-			} else if ((i % 5) == 0) { // i is divisible by 5
-					newFile.println("Buzz");
-				} else { newFile.println(i);; // is is not divisible by 3 or 5
-				}
-			} newFile.close();
+				if(n%3 == 0 && n%5 == 0) // n is divisible by 3 and 5, output "FizzBuzz" to file
+					writer.println("FizzBuzz");
+				else if(n%3 == 0 || (n+"").contains("3")) // n is divisible by 3 or n contains 3, output "Fizz"
+					writer.println("Fizz");
+				else if(n%5 == 0 || (n+"").contains("5")) // n is divisible by 5 or n contains 5, output "Buzz"
+					writer.println("Buzz");
+				else // else output n
+					writer.println(n);
+			}
+
+			writer.close(); // close the file
+
 
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+// invalid destination file, display error and exit
+			System.out.println("ERROR: Invalid destination file.");
 		}
+	}
+
+//		// create new file with given name
+//		try {
+//			PrintWriter newFile = new PrintWriter(destinationFile);
+//
+//			// loop from 1-300
+//			for (int i = 0; i < 300; i++)
+//			{
+//				if(i%3 == 0 && i%5 == 0) { // i is divisible by 3 and 5
+//					newFile.println("FizzBuzz");
+//				} else if (i%3 == 0) { // i is divisible by 3
+//					newFile.println("Fizz");
+//			} else if ((i % 5) == 0) { // i is divisible by 5
+//					newFile.println("Buzz");
+//				} else { newFile.println(i);; // is is not divisible by 3 or 5
+//				}
+//			} newFile.close();
+//
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
 
 
 
 	}
 
-}
