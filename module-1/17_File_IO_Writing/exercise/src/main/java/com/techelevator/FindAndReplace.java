fa2a43aadebeefbb7c905aeffc05fde54595bc12
Marcus Patterson <mpatterson8oh@gmail.com>
2023-03-02 08:41:31 -0500

    exercises

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -10,38 +10,36 @@ public class FindAndReplace {
     public static void main(String[] args) {
 // create scanner object
         Scanner input = new Scanner(System.in);
-// Read all information from the user
+// Read information from the user
         System.out.println("What is the search word?");
         String search = input.nextLine();
         System.out.println("What is the replacement word?");
         String replacementWord = input.nextLine();
         System.out.println("What is the source file?");
-        String source = input.nextLine();
+        String file = input.nextLine();
         System.out.println("What is the destination file?");
         String destination = input.nextLine();
-        input.close();
+
         try {
 // create FileWriter object
             FileWriter fileOut = new FileWriter(destination);
 // create BufferedWriter object
-            BufferedWriter bw = new BufferedWriter(fileOut);
+            BufferedWriter newFile = new BufferedWriter(fileOut);
 
 // create scanner object
-            Scanner fileRead = new Scanner(new File(source));
+            Scanner fileRead = new Scanner(new File(file));
 // check next line present or not
             while (fileRead.hasNextLine()) {
 // read each line
                 String data = fileRead.nextLine();
 // write data to the destination file
-                bw.write(data.replace(search, replacementWord));
-                bw.newLine();
+                newFile.write(data.replace(search, replacementWord));
+                newFile.newLine();
             }
-            bw.flush();
-            bw.close();
+            newFile.close();
             fileRead.close();
             System.out.println("Data replaced successfully.");
         } catch (IOException e) {
-            System.out.println(e.getMessage());
         }
 
     }
