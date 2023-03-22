b8206db6a5d94c1bed2b2c7f177040e5cb8af9f0
Marcus Patterson <mpatterson8oh@gmail.com>
2023-03-22 12:06:31 -0400

    API part 1

--- module-2/11_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ module-2/11_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -1,33 +1,41 @@
 package com.techelevator.auctions.services;
 
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.techelevator.auctions.model.Auction;
 
+import java.util.Scanner;
+
 public class AuctionService {
 
     public static String API_BASE_URL = "http://localhost:3000/auctions/";
-    private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate theApiServer = new RestTemplate();
+    private Scanner scanner = new Scanner(System.in);
 
 
     public Auction[] getAllAuctions() {
         // call api here
-        return null;
+
+        return theApiServer.getForObject(API_BASE_URL, Auction[].class);
     }
 
     public Auction getAuction(int id) {
         // call api here
-        return null;
+        return theApiServer.getForObject(API_BASE_URL+id, Auction.class);
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
         // call api here
-        return null;
-    }
 
+           return theApiServer.getForObject(API_BASE_URL + "?title_like=" + title, Auction[].class);
+
+    }
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
         // call api here
-        return null;
+
+        return theApiServer.getForObject(API_BASE_URL + "?" + "currentBid_lte=" + price, Auction[].class);
+
     }
 
 }
